d266c3433ff0056e59dc9badbebd06a24e575409
sillyD <zlidio98@gmail.com>
2021-06-29 00:41:59 +0200

    6. Fetching Other Games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import {popularGamesURL} from '../api';
+import {popularGamesURL, upcomingGamesURL, newGamesURL} from '../api';
 
 // Action creator
 
 export const loadGames = () => async (dispatch) => { // async: to call a function in an asynchronous way
      // FETCH AXIOS
      const popularData = await axios.get(popularGamesURL()); // await: to wait for the data finishing to load
+     const upcomingData = await axios.get(upcomingGamesURL()); 
+     const newGamesData = await axios.get(newGamesURL()); 
      dispatch({
           type: "FETCH_GAMES",
           payload: {
-               popular: popularData.data.results // we get the games directly
+               popular: popularData.data.results, // we get the games directly
+               upcoming: upcomingData.data.results,
+               newGames: newGamesData.data.results
           }
      });
 }
--- src/api.js
+++ src/api.js
@@ -35,7 +35,12 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 // Popular games
 // we get the games since lastYear until now, ordering them starting by the highest rate, and we display 10 games in the page
 const popular_games = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+// we get the games since now until next year, ordering by added, and we display 10 games in the page
+const upcoming_games = `games?key=${API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?key=${API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
 
 //console.log(popularGamesURL());
