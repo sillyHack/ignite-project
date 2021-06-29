15ff53bb158275bc199162d12b340dce8bf999df
sillyD <zlidio98@gmail.com>
2021-06-29 21:08:46 +0200

    9. Global Styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // Components and pages
 import Home from "./pages/Home";
+import GlobalStyle from "./components/GlobalStyles";
 
 function App() {
 
   return (
     <div className="App">
+      <GlobalStyle />
       <Home />
     </div>
   );
