cb92fd922181c50d91615121e2c2761d97ab6ebf
sillyD <zlidio98@gmail.com>
2021-07-02 02:27:43 +0200

    20. Adding Our Nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 // Components and pages
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
+// Styles
 import GlobalStyle from "./components/GlobalStyles";
+// Router
 import {Route} from "react-router-dom";
+
 
 function App() {
   // path={["/game/:id", "/"]} means no matter where we are between "/game/:id" and "/", it gonna display the home page
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <Route path={["/game/:id", "/"]}> 
         <Home />
       </Route>
